957a19478aa6f30a6590b4afc3993f8603034219
wilbur61 <wroales@perscholas.org>
2022-07-20 22:03:21 -0400

    Added my project

---
+++ src/main/java/com/example/AC6demo/Ac6demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AC6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac6demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac6demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AC6demo/Ac6demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AC6demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac6demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
